854e7c90d735d5420a2b764bfdfebac2e4641689
Ahmed Tanvir <mahmedexec@gmail.com>
2022-04-07 02:37:29 +0500

    Add user and video api

--- index.ts
+++ index.ts
@@ -1,71 +1,72 @@
+import axios from "axios";
 import { randomUUID } from "crypto";
 import express from "express";
+import tiktok from "tiktok-app-api";
 
 import puppeteer, { Page } from "puppeteer";
 const app = express();
 
-app.get("/profile/:id", async (req, res) => {
+const requestHeaders = {
+  "User-Agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+};
+
+app.get("/user/:id", async (req, res) => {
   const { id } = req.params;
 
-  if (!id)
-    return res
-      .status(400)
-      .json({ statusCode: 400, type: "error", message: "userId not provided" });
-
   try {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
+    const userPayload = await axios.get(
+      `https://www.tiktok.com/node/share/user/@${id}?user_agent=`,
+      { headers: requestHeaders }
+    );
 
-    page.on("response", async (event) => {
-      try {
-        const val = await event.json();
-
-        val?.userInfo?.user && res.json({ ...val?.userInfo?.user });
-      } catch (err) {}
-    });
-
-    await page.goto(`https://www.tiktok.com/@${id}`);
+    if (!Object.keys(userPayload.data.userInfo).length) {
+      res.status(404).json({ message: "User Not Found!" });
+    }
+    res.json(userPayload.data.userInfo);
   } catch (error) {
     console.log(error);
   }
 });
 
-app.get("/posts/:id", async (req, res) => {
+app.get("/video/:id", async (req, res) => {
   const { id } = req.params;
 
-  const posts: any = [];
+  try {
+    const videoPayload = await axios.get(
+      `https://m.tiktok.com/api/item/detail/?agent_user=&itemId=${id}`,
+      { headers: requestHeaders }
+    );
 
-  if (!id)
-    return res
-      .status(400)
-      .json({ statusCode: 400, type: "error", message: "userId not provided" });
+    if (!Object.keys(videoPayload.data.itemInfo?.itemStruct).length) {
+      res.status(404).json({ message: "User Not Found!" });
+    }
+    res.json(videoPayload.data.itemInfo?.itemStruct);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/videos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { limit = 10 } = req.query;
+  const posts: any = [];
 
   try {
     const browser = await puppeteer.launch({
       defaultViewport: null,
+      // headless: false,
     });
-    const page = await browser.newPage();
 
-    page.on("response", async (event: any) => {
-      try {
-        if (
-          event._headers["content-type"] === "application/json; charset=utf-8"
-        ) {
-          const val = await event.json();
-          const postsPaginated = val?.itemList;
-          postsPaginated && posts.push(...postsPaginated);
-        }
-      } catch (err) {}
-    });
+    const page = await browser.newPage();
 
     await page.goto(`https://www.tiktok.com/@${id}`);
 
-    await scrollPage(page, 1000);
-
-    res.json({ postCount: posts.length, posts });
-  } catch (error) {
-    console.log(error);
-  }
+    res.json({
+      postCount: posts.slice(0, limit).length,
+      posts: posts.slice(0, limit),
+    });
+  } catch (error) {}
 });
 
 app.get("/tag/:id", async (req, res) => {
@@ -101,10 +102,6 @@ app.get("/tag/:id", async (req, res) => {
     });
 
     await scrollPage(page, 10);
-
-    console.log(
-      await page.$$eval("[data-e2e=user-post-item]", (sel) => console.log(sel))
-    );
 
     res.json({ postCount: posts.length, posts });
   } catch (error) {
